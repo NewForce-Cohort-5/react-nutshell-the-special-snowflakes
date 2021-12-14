37c5000c885385d2559f9757ac19789263bc35f1
Andrew Birt <abirt1986@gmail.com>
2021-12-14 11:30:54 -0500

    added basic authentication

---
+++ src/components/auth/Register.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { Form, Button, Modal } from "react-bootstrap";
+import "./Auth.css";
+
+export const Regitser = () => {
+  const [email, setEmail] = useState({});
+  const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+
+  const existingUserCheck = () => {
+    return fetch(`http://localhost:8088/users?email=${email}`)
+    .then(res => res.json())
+    .then(user => !!user.length)
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    existingUserCheck()
+    .then((userExists) => {
+      if (!userExists) {
+        fetch("http://localhost:8088/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email })
+        })
+        .then(res => res.json())
+        .then(createdUser => {
+            if (createdUser.hasOwnProperty("id")) {
+              localStorage.setItem("react_nutshell_user", createdUser.id);
+              navigate("/");
+            }
+        })
+      }
+      else {
+        setShow(true);
+      }
+    });
+  }
+
+  return (
+    <>
+      <Modal 
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+      >
+        <Modal.Header closeButton>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            User already exist!
+          </p>
+        </Modal.Body>
+      </Modal>
+      <Form>
+        <Form.Group>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            You can trust us!
+          </Form.Text>
+          <Form.Text  className="text-muted">
+            Already have an account? <Link to='/login' >Login</Link>
+          </Form.Text>
+        </Form.Group>
+        <Button onClick={handleRegister} variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
+    </>
+  );
+}
