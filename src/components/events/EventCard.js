2505487393b548f73d9ad57a8375f97bc8066509
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-12-14 10:23:13 -0500

    event list created with basic templating for all events

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -1,6 +1,18 @@
-import React from "react"
-import {Link} from "react-router-dom"
+import React from "react";;
+import { Card } from "react-bootstrap/Card";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const EventCard = () => {
-    
+export const EventCard = ({event}) => {
+    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+  <Card.Body>
+    <Card.Title>{event.Title}</Card.Title>
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>Date: {event.date}</ListGroupItem>
+    <ListGroupItem>Location: {event.location}</ListGroupItem>
+  </ListGroup>
+  <Card.Body>
+  </Card.Body>
+</Card>
 }
---
+++ src/components/events/EventList.js
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect } from "react"
+import { EventContext } from "./EventProvider"
+import { EventCard } from "./EventCard"
+import { useNavigate } from "react-router"
+import { Button } from "react-bootstrap/Button"
+
+export const EventList = () => {
+  // This state changes when `getEvents()` is invoked below
+  const { events, getEvents } = useContext(EventContext)
+  const navigate = useNavigate()
+
+  //useEffect - reach out to the world for something
+  useEffect(() => {
+   getEvents()
+  }, [])
+
+
+  return (
+    <>
+    <h2>Events</h2>
+    <Button variant="primary" type="submit" onClick={() => {navigate("/events/create")}}>Create Event</Button>{' '}
+    <div className="events">
+      {
+        events.map(event => {
+          return <EventCard key={event.id} event={event} />
+        })
+      }
+    </div>
+    </>
+  )
+}
